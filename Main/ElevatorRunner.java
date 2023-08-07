8bb22a693b131b753210c122a81d2e74af2fdcb6
PoojaV2912 <139998362+PoojaV2912@users.noreply.github.com>
2023-08-07 20:17:32 +0530

    Add files via upload

---
+++ Main/ElevatorRunner.java
@@ -0,0 +1,15 @@
+package Main;
+
+import Overloading.Elevator;
+
+public class ElevatorRunner {
+
+	public static void main(String[] args) {
+			Elevator elevator=new Elevator();
+			elevator.elevatorInfo("up-down",30,20f);
+			elevator.elevatorInfo();
+
+		}
+
+	}
+	
